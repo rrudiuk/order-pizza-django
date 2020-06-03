69f1275806734af0f369275ee27bf4f28c358d4b
rrudiuk <rus.barik@gmail.com>
2020-06-03 16:47:56 +0300

    First model created

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# Model for Pasta
+class Pasta(models.Model):
+	name = models.CharField(max_length = 64)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+
+	def __str__(self):
+		return f"{self.name}: {self.price}"
