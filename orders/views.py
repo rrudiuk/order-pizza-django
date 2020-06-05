6eb469c8800003ca715dbb57684850405a71d880
rrudiuk <rus.barik@gmail.com>
2020-06-05 19:46:12 +0300

    Started to create users. Index page is now shpwing if you are logged in

--- orders/views.py
+++ orders/views.py
@@ -1,18 +1,38 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Pasta, DinnerPlate, Sub, SubExtra, Salad, Pizza, Topping
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        context = {
+            "pasta": Pasta.objects.all(),
+            "salads": Salad.objects.all(),
+            "dinner_plate": DinnerPlate.objects.all(),
+            "subs": Sub.objects.all(),
+            "subextras": SubExtra.objects.all(),
+            "pizzas": Pizza.objects.all(),
+            "toppings": Topping.objects.all(),
+            "user": "",
+        }
+
+        return render(request, "orders/index.html", context)
+
     context = {
-        "pasta": Pasta.objects.all(),
-        "salads": Salad.objects.all(),
-        "dinner_plate": DinnerPlate.objects.all(),
-        "subs": Sub.objects.all(),
-        "subextras": SubExtra.objects.all(),
-        "pizzas": Pizza.objects.all(),
-        "toppings": Topping.objects.all(),
-    }
+            "pasta": Pasta.objects.all(),
+            "salads": Salad.objects.all(),
+            "dinner_plate": DinnerPlate.objects.all(),
+            "subs": Sub.objects.all(),
+            "subextras": SubExtra.objects.all(),
+            "pizzas": Pizza.objects.all(),
+            "toppings": Topping.objects.all(),
+            "user": request.user,
+        }
 
     return render(request, "orders/index.html", context)
+
+def login_view(request):
+    return render(request, "orders/login.html")
